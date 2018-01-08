327e72174f9fdcaf8175999f791123cc69b3c639
Limingyuan <894777832@qq.com>
2018-01-08 11:11:33 +0800

    日历的头部切换动作

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CalendarContent from './calendar/CalendarContent';
+import CalendarHeader from './calendar/CalendarHeader';
 
 class App extends Component {
+  constructor(){
+    super();
+    const now = new Date();
+    this.state = {
+      year: now.getFullYear(),
+      month: now.getMonth(),
+    }
+  }
+
+  /**
+   * 切换到上一个月
+   */
+  prevMonth(){
+    const month = this.state.month === 0 ? 11 : this.state.month - 1;
+    const year = this.state.month === 0 ? this.state.year - 1 : this.state.year;
+
+    this.setState({
+      year,
+      month,
+    })
+  }
+
+   /**
+   * 切换到下一个月
+   */
+  nextMonth(){
+    const month = this.state.month === 11 ? 0 : this.state.month + 1;
+    const year = this.state.month === 11 ? this.state.year + 1 : this.state.year;
+
+    this.setState({
+      year,
+      month,
+    })
+  }
+
+  
+
+
+
   render() {
     return (
       <div className="App">
-        <CalendarContent />
+        <CalendarHeader 
+          year={this.state.year}  
+          month={this.state.month}
+          prevMonth={() => this.prevMonth()}
+          nextMonth={() => this.nextMonth()}
+        />
       </div>
     );
   }
