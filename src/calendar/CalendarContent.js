b7604b87a8dce1ac28be4b98a3fea3a55bbe1321
yangqi <863403666@qq.com>
2018-01-08 10:16:27 +0800

    安装styled

--- src/calendar/CalendarContent.js
+++ src/calendar/CalendarContent.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+`;
 
 export default function CalendarContent() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>日</th>
-          <th>一</th>
-          <th>二</th>
-          <th>三</th>
-          <th>四</th>
-          <th>五</th>
-          <th>六</th>
-        </tr>
-      </thead>
-      <tbody></tbody>
-    </table>
+    <div>
+      <Title>日历</Title>
+      <table>
+        <thead>
+          <tr>
+            <th>日</th>
+            <th>一</th>
+            <th>二</th>
+            <th>三</th>
+            <th>四</th>
+            <th>五</th>
+            <th>六</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
   );
 }
