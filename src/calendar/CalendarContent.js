cf6a0c5d25dd1fe095a12a2362a7179a0369b53d
yangqi <863403666@qq.com>
2018-01-08 11:15:40 +0800

    更改日历

--- src/calendar/CalendarContent.js
+++ src/calendar/CalendarContent.js
@@ -5,24 +5,61 @@ const Title = styled.h1`
   color: red;
 `;
 
-export default function CalendarContent() {
-  return (
-    <div>
-      <Title>日历</Title>
-      <table>
-        <thead>
-          <tr>
-            <th>日</th>
-            <th>一</th>
-            <th>二</th>
-            <th>三</th>
-            <th>四</th>
-            <th>五</th>
-            <th>六</th>
-          </tr>
-        </thead>
-        <tbody></tbody>
-      </table>
-    </div>
-  );
+const Table = styled.table`
+  width:100%;
+  height:auto;
+  border-collapse: collapse;
+`;
+
+const Thead = styled.thead`
+
+`;
+
+const Tr = styled.tr`
+  width:100%;
+  height:30px;
+`;
+
+const Th = styled.th`
+  width:14.28%;
+  height:auto;
+  border:1px solid #dedede;
+`;
+
+const Tbody = styled.tbody`
+
+`;
+
+export default class CalendarContent extends React.Component {
+  render(){
+    return (
+      <div>
+        <Title>日历</Title>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>日</Th>
+              <Th>一</Th>
+              <Th>二</Th>
+              <Th>三</Th>
+              <Th>四</Th>
+              <Th>五</Th>
+              <Th>六</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Th></Th>
+              <Th>1</Th>
+              <Th>2</Th>
+              <Th>3</Th>
+              <Th>4</Th>
+              <Th>5</Th>
+              <Th>6</Th>
+            </Tr>
+          </Tbody>
+        </Table>
+      </div>
+    );
+  }
 }
